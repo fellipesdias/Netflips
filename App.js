3b58f452b8b670bcf35649ac10c6b8c22b3a482c
fellipesdias <62446990+fellipesdias@users.noreply.github.com>
2020-03-20 18:34:11 -0300

    primeiro commit - tela de login

---
+++ App.js
@@ -0,0 +1,130 @@
+import React, {useState} from 'react';
+import {
+  Text,
+  View,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  ActivityIndicator,
+  Linking,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+const App = () => {
+  const [checked, setChecked] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  return (
+    <View nativeID="container" style={styles.container}>
+      <View nativeID="header" style={styles.header}>
+        <Text style={styles.headerText}>ENTRAR</Text>
+      </View>
+      <View nativeID="body" style={styles.form}>
+        <TextInput
+          returnKeyType="next"
+          keyboardType="email-address"
+          style={styles.input}
+          placeholder="Email ou Número de telefone"
+          placeholderTextColor="#9a9a9a"
+        />
+        <TextInput
+          returnKeyType="go"
+          secureTextEntry
+          style={styles.input}
+          placeholder="Senha"
+          placeholderTextColor="#9a9a9a"
+        />
+
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => {
+            setLoading(!loading);
+          }}>
+          {loading ? (
+            <ActivityIndicator size="small" color="#fff" />
+          ) : (
+            <Text style={styles.buttonText}>Entrar</Text>
+          )}
+        </TouchableOpacity>
+        <View style={[styles.LoginIconView, styles.alignIconView]}>
+          <View
+            onTouchStart={() => {
+              setChecked(!checked);
+            }}
+            style={styles.LoginIconView}>
+            {checked ? (
+              <Icon name="check-square" size={20} color="#fff" />
+            ) : (
+              <Icon name="square" size={20} color="#fff" />
+            )}
+            <Text style={styles.LoginIconText}>Lembre-se de mim</Text>
+          </View>
+          <Text
+            style={styles.loginUrl}
+            onPress={() => Linking.openURL('http://google.com')}>
+            Precisa de ajuda ?
+          </Text>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  header: {
+    backgroundColor: '#000000',
+    height: 70,
+    justifyContent: 'center',
+    paddingLeft: 30,
+    paddingVertical: 50,
+  },
+  headerText: {
+    color: '#fff',
+    fontSize: 26,
+  },
+  form: {
+    justifyContent: 'flex-start',
+    flex: 1,
+    backgroundColor: '#000000',
+    paddingHorizontal: 20,
+  },
+  input: {
+    backgroundColor: '#353839',
+    marginVertical: 10,
+    height: 65,
+    paddingLeft: 20,
+    color: '#fff',
+  },
+  button: {
+    backgroundColor: 'red',
+    marginVertical: 10,
+    height: 65,
+    marginTop: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 20,
+  },
+  LoginIconView: {
+    flexDirection: 'row',
+  },
+  alignIconView: {
+    justifyContent: 'space-between',
+  },
+  LoginIconText: {
+    fontSize: 16,
+    color: 'gray',
+    marginLeft: 10,
+  },
+  loginUrl: {
+    fontSize: 16,
+    color: 'gray',
+  },
+});
